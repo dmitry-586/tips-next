2eec912053e951b5f175412b22ed2d350288b369
Dmitry Kutsevalov <kutdmitry@gmail.com>
2024-08-29 17:48:13 +0300

    full adaptive

--- src/components/aboutHeader/aboutHeader.tsx
+++ src/components/aboutHeader/aboutHeader.tsx
@@ -1,11 +1,11 @@
 import Button from "../button/Button";
 import Image from "next/image";
-import styles from "../../styles/aboutHeader.module.scss"
+import styles from "../../styles/aboutHeader.module.scss";
 
 export default function AboutHeader() {
   return (
     <div className={styles.about}>
-      <div>
+      <div className={styles.about_left}>
         <div className={styles.about_left_top}>
           <div className={styles.about_left_top_image}>
             <Image
@@ -19,12 +19,28 @@ export default function AboutHeader() {
               }}
             />
           </div>
+          <div className={styles.about_left_top_image2}>
+            <Image
+              width={203}
+              height={0}
+              src="/images/about/AboutLine2.svg"
+              alt="AboutLine"
+              style={{
+                height: "auto",
+                width: "500px",
+              }}
+            />
+          </div>
           <h2>О&nbsp;студии</h2>
         </div>
         <div className={styles.about_left_bottom}>
           <p>
             Наша команда опытных специалистов работает над созданием продающих и
             функциональных веб-решений для различных клиентов
+          </p>
+          <p className={styles.left_p}>
+            Обеспечиваем полную прозрачность на всех этапах работы, чтобы вы
+            могли быть уверены в том, что ваш проект находится в надежных руках
           </p>
           <Button
             children="Получить бесплатную структуру"
