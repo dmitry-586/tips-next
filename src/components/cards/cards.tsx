78c46357564432a172e701edff629a1e4eb39b84
Dmitry Kutsevalov <kutdmitry@gmail.com>
2024-09-10 22:26:57 +0300

    pravki 18

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,7 +1,4 @@
-import Card from "../cards/card/card";
 import Circle from "../circle/circle";
-import styles from "../../styles/cards.module.scss";
-import ButtonComponent from "../button/Button";
 import Dropdown from "../info/drop.info";
 import { useTranslations } from "next-intl";
 import CardsFilter from "./cards.filter";
@@ -69,68 +66,6 @@ export default function Cards() {
   return (
     <div className="relative" id="portfolio">
       <CardsFilter buttons={buttons} cards={cards} />
-      {/* карточки */}
-      {/* <div className={styles.cards_wrapper}>
-        <Card
-          title={t("title1")}
-          info={t("info1")}
-          imageSrc="/images/cards/card1.webp"
-          width={554}
-          height={337}
-          padding="px-[30px]"
-          href="https://nkstore.pro/"
-        />
-        <Card
-          className="bg-gradient-card2"
-          title={t("title2")}
-          info={t("info2")}
-          imageSrc="/images/cards/card2.webp"
-          width={553}
-          height={335}
-          padding="px-[40px]"
-          href="https://obraz-strah.ru/"
-        />
-        <Card
-          className="bg-gradient-card3"
-          title={t("title2")}
-          info={t("info3")}
-          imageSrc="/images/cards/card3.webp"
-          width={544}
-          height={335}
-          padding="px-[40px]"
-          href="https://автониндзя.рф"
-        />
-        <Card
-          className="bg-gradient-card4"
-          title={t("title2")}
-          info={t("info4")}
-          imageSrc="/images/cards/card4.webp"
-          width={538}
-          height={333}
-          padding="px-[40px]"
-          href="https://il-gusto58.ru/"
-        />
-        <Card
-          className="bg-gradient-card5"
-          title={t("title1")}
-          info={t("info5")}
-          imageSrc="/images/cards/card5.png"
-          width={531}
-          height={332}
-          padding="px-[40px]"
-          href="https://illiadi.com/"
-        />
-        <Card
-          className="bg-gradient-card6"
-          title={t("title2")}
-          info={t("info6")}
-          imageSrc="/images/cards/card6.png"
-          width={526}
-          height={333}
-          padding="px-[40px]"
-          href="https://evencoarse.com/"
-        />
-      </div> */}
 
       {/* <Dropdown
         openHeader={styles.openHeader}
