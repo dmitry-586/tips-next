ca98eff94190a4fb54b79e1bc06983238b13c062
egortrushenkov <etrushenkov@gmail.com>
2024-09-14 16:29:36 +0300

    fix favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -56,7 +56,7 @@ export const metadata: Metadata = {
     url: "https://webstudio-tips.ru",
     images: [
       {
-        url: "../favicon.ico",
+        url: "favicon.ico",
         width: 256,
         height: 256,
       },
