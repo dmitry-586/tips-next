265d3e79a86555cb0c53954cf1965336d42ebb66
egortrushenkov <etrushenkov@gmail.com>
2024-09-15 16:03:15 +0300

    fix fail

--- src/components/YandexMetrika/YandexMetrika.tsx
+++ src/components/YandexMetrika/YandexMetrika.tsx
@@ -1,17 +1,22 @@
 'use client'
- 
+
 import { useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
- 
+
+declare global {
+  interface Window {
+    ym: (id: number, type: string, url: string) => void
+  }
+}
+
 export default function YandexMetrika() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
- 
+
   useEffect(() => {
     const url = `${pathname}?${searchParams}`
-    ym(98342954, 'hit', url);
- 
+    window.ym(98342954, 'hit', url)
   }, [pathname, searchParams])
- 
+
   return null
 }
